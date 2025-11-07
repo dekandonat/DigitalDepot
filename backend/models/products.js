94658c7e6de00af4af35e755d368ea360839ebb5
dekandonat <dekandoni@gmail.com>
2025-11-07 12:53:40 +0100

    termékekhez tartozó API-k létrehozva

--- backend/models/products.js
+++ backend/models/products.js
@@ -1,4 +1,19 @@
-let products = [];
+let products = [
+    {
+    "prodName": "Wired Mouse",
+    "prodId": 1,
+    "prodDescription": "Ergonomic wired mouse with adjustable DPI.",
+    "prodPrice": 25.99,
+    "prodImg": "https://example.com/images/wireless-mouse.jpg"
+  },
+  {
+    "prodName": "Mechanical Keyboard",
+    "prodId": 2,
+    "prodDescription": "RGB mechanical keyboard with tactile switches and customizable lighting.",
+    "prodPrice": 79.99,
+    "prodImg": "https://example.com/images/mechanical-keyboard.jpg"
+  }
+];
 
 module.exports = class Products{
     constructor(prodName, prodId, prodDescription, prodPrice, prodImg){
--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,37 @@
 const express = require('express');
+const cors = require("cors");
 
 const app = express();
 
 const IP = "localhost";
 const PORT = 3000;
 
+const Products = require('./models/products');
+
+app.use(express.json());
+app.use(cors());
+
+app.post('/products', (req, res) => {
+    const product = new Products(req.body.prodName, req.body.prodId , req.body.prodDescription, req.body.prodPrice, req.body.prodImg);
+    product.save();
+    res.status(201).json({message: "product saved!"});
+});
+
+app.get('/products/:prodId', (req, res) => {
+    const id = req.params.prodId;
+    const product = Products.fetchAll().find(prod => prod.prodId == id);
+
+    if(!product){
+        return res.status(404).json({message: "Nincs ilyen termék!"});
+    }
+
+    res.json(product);
+});
+
+app.get('/products', (req, res) => {
+    res.json(Products.fetchAll());
+});
+
 app.listen(PORT, IP, ()=>{
     console.log(`Server running on: ${IP}:${PORT}`);
-})
+});
