a2b67b82d4349ded82c22ed5e04f1e271e3f1286
dekandonat <dekandoni@gmail.com>
2025-11-11 22:47:51 +0100

    prettier config file hozzáadva

--- backend/models/products.js
+++ backend/models/products.js
@@ -1,42 +1,43 @@
 const db = require('../util/database');
 
-module.exports = class Products{
-    constructor(prodName, prodDescription, prodPrice, prodImg){
-        this.prodName = prodName;
-        this.prodDescription = prodDescription;
-        this.prodPrice = prodPrice;
-        this.prodImg = prodImg;
-    }
+module.exports = class Products {
+  constructor(prodName, prodDescription, prodPrice, prodImg) {
+    this.prodName = prodName;
+    this.prodDescription = prodDescription;
+    this.prodPrice = prodPrice;
+    this.prodImg = prodImg;
+  }
 
-    async save(){ 
-        try{
-            await db.execute(`INSERT INTO products (productName, productDescription, productPrice, productImg) VALUES ("${this.prodName}", "${this.prodDescription}", ${this.prodPrice}, "${this.prodImg}")`);
-            return {result: "success"};
-        }
-        catch(err){
-            return {result: "fail", message: err.message};
-        }
+  async save() {
+    try {
+      await db.execute(
+        `INSERT INTO products (productName, productDescription, productPrice, productImg) VALUES ("${this.prodName}", "${this.prodDescription}", ${this.prodPrice}, "${this.prodImg}")`
+      );
+      return { result: 'success' };
+    } catch (err) {
+      return { result: 'fail', message: err.message };
     }
+  }
 
-    static async fetchAll(){
-        try{
-            const [rows] = await db.execute('SELECT * FROM products');
-            console.log(rows);
-            return rows;
-        }
-        catch(err){
-            throw err;
-        }
+  static async fetchAll() {
+    try {
+      const [rows] = await db.execute('SELECT * FROM products');
+      console.log(rows);
+      return rows;
+    } catch (err) {
+      throw err;
     }
+  }
 
-    static async fetch(id){
-        try{
-            const [rows] = await db.execute(`SELECT * FROM products WHERE prodId = ${id}`);
-            console.log(rows);
-            return rows;
-        }
-        catch(err){
-            throw err;
-        }
+  static async fetch(id) {
+    try {
+      const [rows] = await db.execute(
+        `SELECT * FROM products WHERE prodId = ${id}`
+      );
+      console.log(rows);
+      return rows;
+    } catch (err) {
+      throw err;
     }
-}
+  }
+};
--- backend/util/database.js
+++ backend/util/database.js
@@ -3,10 +3,10 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const db = mysql.createPool({
-    host: process.env.HOST,
-    user: process.env.USER,
-    password: process.env.PASSWORD,
-    database: process.env.DATABASE 
+  host: process.env.HOST,
+  user: process.env.USER,
+  password: process.env.PASSWORD,
+  database: process.env.DATABASE,
 });
 
 module.exports = db;
