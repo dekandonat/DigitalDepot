f9c8927e521f134a2050b9e4dce920d29a8fd488
dekandonat <dekandoni@gmail.com>
2026-05-27 23:24:33 +0200

    Reapply "Merge branch '50-utolsó-simítások-hozzáadása'"
    This reverts commit cd7e45a11aec59b88b9dd563439cbcc6a22e2249.

--- backend/util/database.js
+++ backend/util/database.js
@@ -3,7 +3,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const db = mysql.createPool({
-  host: process.env.HOST,
+  host: process.env.DB_HOST || process.env.HOST,
   user: process.env.USER,
   password: process.env.PASSWORD,
   database: process.env.DATABASE,
